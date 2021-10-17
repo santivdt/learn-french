013e869f05a4a9aafdd08345667ae289ae3ee1bf
Santi van den Toorn <santi.vdt@gmail.com>
2021-10-17 14:16:18 +0200

    Remove MUI Cards, change NL/LAT to LAT/IMG

--- pages/index.js
+++ pages/index.js
@@ -2,135 +2,127 @@ import styles from '../styles/Home.module.css'
 import React, { useState } from "react"
 import {button} from '../styles/Button.module.css'
 import Layout from '../components/layout/layout.js'
-import { createTheme } from '@material-ui/core/styles'
 import { data } from '../data/data'
+import Image from 'next/image'
 import {
     Switch,
     Button,
-    Box,
-    Card,
-    CardActions,
-    CardContent,
-    Typography,
-    CardHeader,
-    Avatar,
-    IconButton,
-    CardMedia,
-    Collapse
 } from "@material-ui/core"
 
 
 export default function Home() {
 
-    const randomArrayShuffle = (array) => {
-        let currentIndex = array.length, temporaryValue, randomIndex;
+    const randomShuffleArray = (array) => {
+        console.log('array', array)
+        let currentIndex = array.length, temporaryValue, randomIndex
         while (0 !== currentIndex) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex -= 1
+            temporaryValue = array[currentIndex]
+            array[currentIndex] = array[randomIndex]
+            array[randomIndex] = temporaryValue
         }
-        return array;
+        return array
     }
 
-    const randomData = randomArrayShuffle(data)
+    const [memoryStatus, setMemoryStatus] = useState(data)
 
-    const [memoryStatus, setMemoryStatus] = useState(randomData)
+    //TO DO works but does not seem to update the state?
+    const changeOrder = (array) => {
+        const newMem = randomShuffleArray(array)
+        setMemoryStatus(newMem)
+    }
 
-    const [whichOne, setWhichOne] = useState(true)
+    const [showName, setShowName] = useState(false)
 
     const handleChange = (position) => {
-        const newMemoryStatus = memoryStatus.map((item, index) => {
+        console.log('handlechange')
+        let newMemoryStatus = memoryStatus.map((item, index) => {
             if(index === position) {
-                return {...item, showNormal: !item.showNormal}
+                return {...item, showName: !item.showName}
             }
             else return item
         })
-
         setMemoryStatus(newMemoryStatus)
-        console.log(memoryStatus)
-    }
-
-    const showLatin = () => {
-        const showNormalFalse = memoryStatus.map(o => ({ ...o, showNormal: false }))
-        return (
-            setMemoryStatus(showNormalFalse),
-            setWhichOne(false)
-        )
-    }
-
-    const showNormal = () => {
-        const showNormalTrue = memoryStatus.map(o => ({ ...o, showNormal: true }))
-        return (
-            setMemoryStatus(showNormalTrue),
-            setWhichOne(true)
-    )
     }
 
     const resetCards = () => {
-        if (whichOne) {
-            const setAllTrue = memoryStatus.map(o => ({ ...o, showNormal: true }))
+        console.log('reset cards')
+        if (showName) {
+            let setAllTrue = memoryStatus.map(o => ({ ...o, showName: true }))
             return setMemoryStatus(setAllTrue)
         } else {
-            const setAllFalse = memoryStatus.map(o => ({ ...o, showNormal: false }))
+            let setAllFalse = memoryStatus.map(o => ({ ...o, showName: false }))
             return setMemoryStatus(setAllFalse)
         }
     }
 
+    const changeSwitch = () => {
+        console.log('changeswitch')
+        if(showName) {
+            let showNameFalse = memoryStatus.map(o => ({ ...o, showName: false }))
+            return (
+                setMemoryStatus(showNameFalse),
+                    setShowName(false)
+            )
+        } else {
+            let showNameTrue = memoryStatus.map(o => ({ ...o, showName: true }))
+            return (
+                setMemoryStatus(showNameTrue),
+                    setShowName(true)
+            )
+        }
+    }
+
     return (
-    <Layout pageTitle="home">
-        <div className={styles.buttonContainer}>
-              {whichOne &&  <p>    <Button
-                  variant="contained"
-                  color="primary"
-                  className={button}
-                  onClick={() => {showLatin()}}
-              >
-                  Show Latin
-              </Button>
-              </p>
-              }
-              {!whichOne &&  <p>    <Button
-                  variant="contained"
-                  color="primary"
-                  className={button}
-                  onClick={() => {showNormal()}}
-              >
-                  Show Normal
-              </Button>
-              </p>
-              }
-            <Button
-                variant="contained"
-                color="primary"
-                className={button}
-                onClick={() => {resetCards()}}
-            >
-                Reset cards
-            </Button>
-        </div>
-        <div className={styles.container}>
+        <Layout>
+            <div className={styles.buttonContainer}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    className={button}
+                    onClick={() => {resetCards()}}
+                >
+                    Reset cards
+                </Button>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    className={button}
+                    onClick={() => {changeOrder(memoryStatus)}}
+                >
+                    Change order
+                </Button>
+                <div>
+                    <Switch
+                        color="primary"
+                        onChange={() => {changeSwitch()}}/>
+                </div>
+            </div>
+            <div className={styles.container}>
+                {memoryStatus.map((item, index) => {
+                    return (
+                        <div
+                            key={index}
+                            onClick={() => {handleChange(index)}}
+                            className={styles.card}
+                        >
+                            {!item.showName && <Image
+                                src={item.img}
+                                width="100px"
+                                height="100px"
+                            />}
+                            <span>
+                                <center>
+                                    {item.showName ? item.latinName : ''}
+                                </center>
+                            </span>
 
-              {memoryStatus.map((item, index) => {
-                  const flipSide = item.showNormal ? styles.showNormal : styles.showLatin
-                  return (
+                        </div>
 
-                      <div
-                          className={`${styles.card} ${flipSide}`}
-                          key={index}
-                          onClick={() => handleChange(index)}
-                      >
-                        <span>
-                            {item.showNormal && <p>{item.name}</p>}
-                            {!item.showNormal && <p>{item.latinName}</p>}
-                        </span>
-                      </div>
-                  )
-              })}
-        </div>
-    </Layout>
-
-
+                    )})
+                }
+            </div>
+        </Layout>
   )
 }
