1cb5e2edd89b4261ca4ee0ab7aeb017299475da0
Giel <g.cobben@gmail.com>
2021-12-27 18:09:21 +0100

    Work

--- pages/editdata.js
+++ pages/editdata.js
@@ -1,263 +1,205 @@
-import { useState, useEffect } from 'react'
-import initFirebase from '../firebase/initFirebase.js'
-import firebase from 'firebase'
-import {
-    TextField,
-} from '@mui/material'
-import WarningDialog from '../components/dialog.js'
-import styles from '../styles/editdata.module.scss'
+import { useState, useEffect } from "react";
+import initFirebase from "../firebase/initFirebase.js";
+import firebase from "firebase";
+import { TextField } from "@mui/material";
+import WarningDialog from "../components/dialog.js";
+import styles from "../styles/editdata.module.scss";
+
+const doSearch = (nameKey, myArray) => {
+  const results = [];
+  for (var i = 0; i < myArray.length; i++) {
+    if (
+      myArray[i].name.includes(nameKey) ||
+      myArray[i].latinName.includes(nameKey)
+    ) {
+      results.push(myArray[i]);
+    }
+  }
+  return results;
+};
 
 export default function Editdata() {
-    const [allData, setAllData] = useState([])
-    const [filteredData, setFilteredData] = useState([])
-    const [editIdx, setEditIdx] = useState(-1)
-    const [value, setValue] = useState({ name: '', latinName: '' })
-    const [open, setOpen] = useState(false)
-    const [itemToDeleteName, setItemToDeleteName] = useState('')
-    const [itemToDeleteKey, setItemToDeleteKey] = useState('')
-    const [status, setStatus] = useState('')
+  const [plants, setPlants] = useState([]);
+  const [status, setStatus] = useState(null);
+  const [editName, setEditName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [editLatinName, setEditLatinName] = useState("");
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const items = search.length > 0 ? results : plants;
 
-    const handleClickCancel = () => {
-        setOpen(false)
+  //   const handleRemove = async (doc) => {
+  //     firebase
+  //       .firestore()
+  //       .collection("plants")
+  //       .doc(doc)
+  //       .delete()
+  //       .then(() => {
+  //         console.log("yes"),
+  //           setStatus("Your item has been deleted"),
+  //           setTimeout(() => {
+  //             setStatus("");
+  //           }, 2000);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error removing document: ", error);
+  //       });
+  //   };
+
+  const handleSearch = (event) => {
+    setSearch(event.currentTarget.value);
+  };
+
+  const showAlert = () => {
+    setStatus("Your item has been updated");
+
+    setTimeout(() => {
+      setStatus(null);
+    }, 2000);
+  };
+
+  const edit = (id) => {
+    const newPlants = plants.map((item) => ({
+      ...item,
+      isEditing: item.id === id ? true : false,
+    }));
+    setPlants(newPlants);
+
+    const item = plants.find((item) => item.id === id);
+    setEditName(item.name);
+    setEditLatinName(item.latinName);
+  };
+
+  const save = async (id) => {
+    try {
+      setLoading(true);
+      await firebase
+        .firestore()
+        .collection("plants")
+        .doc(id)
+        .set({ name: editName, latinName: editLatinName });
+
+      const newPlants = [...plants];
+      const plantIndex = newPlants.findIndex((item) => item.id === id);
+
+      newPlants[plantIndex].isEditing = false;
+      newPlants[plantIndex].name = editName;
+      newPlants[plantIndex].latinName = editLatinName;
+
+      setPlants(newPlants);
+      setLoading(false);
+      showAlert();
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
     }
+  };
 
-    const handleClickOpen = (doc, name) => {
-        setItemToDeleteKey(doc)
-        setItemToDeleteName(name)
-        setOpen(true)
-    }
+  const getPlants = async () => {
+    const p = [];
+    setLoading(true);
+    const querySnapshot = await firebase.firestore().collection("plants").get();
+    querySnapshot.forEach((doc) =>
+      p.push({ id: doc.id, showName: false, isEditing: false, ...doc.data() })
+    );
+    setPlants(p);
+    setLoading(false);
+  };
 
-    const handleClickOke = () => {
-        handleRemove(itemToDeleteKey)
-        setItemToDeleteName('')
-        setItemToDeleteKey('')
-        setOpen(false)
-    }
+  useEffect(() => {
+    initFirebase();
+    getPlants();
+  }, []);
 
-    useEffect(() => {
-        initFirebase()
-        const getData = async () => {
-            let response = []
-            const querySnapshot = await firebase
-                .firestore()
-                .collection('plants')
-                .get()
-            querySnapshot.forEach((doc) => {
-                let temp = doc.data()
-                temp.doc = doc.id
-                response.push(temp)
-            })
+  useEffect(() => {
+    const items = doSearch(search, plants);
+    setResults(items);
+  }, [search]);
 
-            setAllData(response)
-            setFilteredData(response)
-        }
-        getData()
-    }, [])
-
-    const handleSearch = (event) => {
-        console.log('handlesearch')
-        console.log(allData)
-        let value = event.target.value.toLowerCase()
-        let result = []
-
-        console.log(result)
-        // setFilteredData(result)
-    }
-
-    const handleRemove = async (doc) => {
-        firebase
-            .firestore()
-            .collection('plants')
-            .doc(doc)
-            .delete()
-            .then(() => {
-                console.log('yes'),
-                    setStatus('Your item has been deleted'),
-                    setTimeout(() => {
-                        setStatus('')
-                    }, 2000)
-            })
-            .catch((error) => {
-                console.error('Error removing document: ', error)
-            })
-    }
-
-    const startEditing = async (doc, index, item) => {
-        setEditIdx(index)
-
-        setValue({
-            name: item.name,
-            latinName: item.latinName,
-        })
-    }
-
-    const writeToFireBaseStore = (doc) => {
-        try {
-            firebase
-                .firestore()
-                .collection('plants')
-                .doc(doc)
-                .set(value)
-                .then(() => {
-                    console.log('yes'),
-                        setStatus('Your item has been updated'),
-                        setTimeout(() => {
-                            setStatus('')
-                        }, 2000)
-                })
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const stopEditing = (key, i) => {
-        writeToFireBaseStore(key)
-        setEditIdx(-1)
-        setValue({ name: '', latinName: '' })
-    }
-
-    const handleChange = (e, type) => {
-        if (type === 'name') {
-            setValue({ ...value, name: e })
-        } else if (type === 'latinName') {
-            setValue({ ...value, latinName: e })
-        }
-    }
-
-    const checkIfCurrentlyEditing = (index, editIdx) => {
-        return index === editIdx
-    }
-
-    return (
-        <>
-            <TextField
-                    id="standard-basic"
-                    label="Search"
-                    variant="standard"
-                    onChange={(event) => handleSearch(event)}
-                    className={styles.search}
-                />
-                <WarningDialog
-                    title="Are you sure?"
-                    handleClickCancel={handleClickCancel}
-                    handleClickOke={handleClickOke}
-                    state={open}
-                    itemToDeleteKey={itemToDeleteKey}
-                >
-                    Are you sure you want to delete {itemToDeleteName}?
-                </WarningDialog>
-            <span className={styles.status}>{status}</span>
-            <table>
-                    <thead>
-                        <tr>
-                            <th>
-                                Name
-                            </th>
-                            <th>                                
-                                Latin
-                            </th>                            
-                            <th>                                
-                                Edit
-                            </th>                            
-                            <th>                                
-                                Delete
-                            </th>
-                        </tr>
-                    </thead>
-                <tbody>
-                    {allData.map((item, index) => {
-                        const currentlyEditing = checkIfCurrentlyEditing(
-                            index,
-                            editIdx
-                        )
-
-                        return (
-                            <tr
-                                key={index}
-                                sx={{
-                                    '&:last-child td, &:last-child th': {
-                                        border: 0,
-                                    },
-                                }}
-                            >
-                                <td>
-                                    {!currentlyEditing ? (
-                                        item.name
-                                    ) : (
-                                        <TextField
-                                            name={item.name}
-                                            value={value.name}
-                                            onChange={(e) =>
-                                                handleChange(
-                                                    e.target.value,
-                                                    'name'
-                                                )
-                                            }
-                                        />
-                                    )}
-                                </td>
-                                <td>
-                                    {!currentlyEditing ? (
-                                        item.latinName
-                                    ) : (
-                                        <TextField
-                                            name={item.latinName}
-                                            value={value.latinName}
-                                            onChange={(e) =>
-                                                handleChange(
-                                                    e.target.value,
-                                                    'latinName'
-                                                )
-                                            }
-                                        />
-                                    )}
-                                </td>
-                                <td>
-                                    {!currentlyEditing ? (
-                                        <button
-                                            
-                                            onClick={() =>
-                                                startEditing(
-                                                    item.doc,
-                                                    index,
-                                                    item
-                                                )
-                                            }
-                                        >
-                                            Edit
-                                        </button>
-                                    ) : (
-                                        <button
-                                            
-                                            onClick={() =>
-                                                stopEditing(item.doc, index)
-                                            }
-                                        >
-                                            Save
-                                        </button>
-                                    )}
-                                </td>
-                                <td>
-                                    <button
-                                        
-                                        disabled={currentlyEditing}
-                                        onClick={() =>
-                                            handleClickOpen(
-                                                item.doc,
-                                                item.name
-                                            )
-                                        }
-                                    >
-                                        <span className="material-icons">
-                                            delete
-                                        </span>
-                                    </button>
-                                </td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </>
-    )
+  return (
+    <>
+      <TextField
+        id="standard-basic"
+        label="Search"
+        variant="standard"
+        className={styles.search}
+        onChange={handleSearch}
+        value={search}
+      />
+      {/* <WarningDialog
+        title="Are you sure?"
+        handleClickCancel={handleClickCancel}
+        handleClickOke={handleClickOke}
+        state={open}
+        itemToDeleteKey={itemToDeleteKey}
+      >
+        Are you sure you want to delete {itemToDeleteName}?
+      </WarningDialog> */}
+      {status && <span className={styles.status}>{status}</span>}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Latin</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, index) => {
+            return (
+              <tr
+                key={index}
+                sx={{
+                  "&:last-child td, &:last-child th": {
+                    border: 0,
+                  },
+                }}
+              >
+                <td>
+                  {item.isEditing ? (
+                    <TextField
+                      name={item.name}
+                      value={editName}
+                      onChange={(event) =>
+                        setEditName(event.currentTarget.value)
+                      }
+                    />
+                  ) : (
+                    item.name
+                  )}
+                </td>
+                <td>
+                  {item.isEditing ? (
+                    <TextField
+                      name={item.latinName}
+                      value={editLatinName}
+                      onChange={(e) => setEditLatinName(e.currentTarget.value)}
+                    />
+                  ) : (
+                    item.latinName
+                  )}
+                </td>
+                <td>
+                  {item.isEditing ? (
+                    <button onClick={() => save(item.id)}>Save</button>
+                  ) : (
+                    <button onClick={() => edit(item.id)}>Edit</button>
+                  )}
+                </td>
+                <td>
+                  <button
+                    disabled={item.isEditing}
+                    onClick={() => handleClickOpen(item.doc, item.name)}
+                  >
+                    <span className="material-icons">delete</span>
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
